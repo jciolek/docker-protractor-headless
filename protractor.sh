f1ef97d2f98841126f5c64260260e88e60a9d420
Jacek Ciolek <jacek@webnicer.com>
2017-02-08 14:28:48 +0000

    feat(chrome) upgrade Chrome to version 56; run xvfb as non-root

--- protractor.sh
+++ protractor.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+uid=$(stat -c %u ${PWD})
+gid=$(stat -c %g ${PWD})
 
-xvfb-run -a --server-args="-screen 0 ${SCREEN_RES}" protractor $@
+groupadd -o -g $gid protractor
+useradd -m -o -u $uid -g $gid protractor
+
+# Chrome, starting with version 56, refuses to run when launched by root.
+# Therefore, we need to run it as a regular user, taking care
+# to set the uid and gid of that user to match those of the current directory owner.
+# Otherwise protractor could experience problems reading files from the current directory.
+sudo -u protractor xvfb-run --server-args="-screen 0 ${SCREEN_RES}" protractor $@
